01ed6928cc9c995770a71430a59bbc9e1b9fe262
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-10-13 16:48:28 +0200

    modif

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ function displayList(){
     })
 }
 
+function displayList(){
+    list.innerHTML = ``
+
+    tasks.forEach(function(todo, i){
+        list.innerHTML = list.innerHTML + `
+        <div class="toDoList">
+        <button class="Check"> check ! </button>
+        <h4 class="toDoStatus">${todo.status}</h4>
+        <p class="toDoTexte">${todo.value}</p>
+        <a onClick="edit(${i})"<i class="fas fa-cog"></i>
+        <a  onClick="supprimer(${i})" <i class="fas fa-ban"></i></a>       
+    </div>`
+        
+    })
+}
+
+
 function onTaskSubmit(){
 
     var inputValue = document.getElementById("input").value
