970604499d3d6556305e4ad5ba85931aa4a858fb
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-10-13 18:44:27 +0200

    modif edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var bouton = document.getElementById("bouton")
-var todoContainer = document.getElementsByClassName("todoContainer");
- var list = document.getElementById("list")
+
+var list = document.getElementById("list")
+
 
 var tasks = []
 
@@ -15,28 +15,13 @@ function displayList(){
         <h4 class="toDoStatus">${todo.status}</h4>
         <p class="toDoTexte">${todo.value}</p>
         <a onClick="edit(${i})"<i class="fas fa-cog"></i>
-        <a  onClick="supprimer(${i})" <i class="fas fa-ban"></i></a>       
-    </div>`
+        <a  onClick="supprimer(${i})" <i class="fas fa-ban"></i></a>
+        <div class="modifier"></div>   
+        </div>
+        `
         
     })
 }
-
-function displayList(){
-    list.innerHTML = ``
-
-    tasks.forEach(function(todo, i){
-        list.innerHTML = list.innerHTML + `
-        <div class="toDoList">
-        <button class="Check"> check ! </button>
-        <h4 class="toDoStatus">${todo.status}</h4>
-        <p class="toDoTexte">${todo.value}</p>
-        <a onClick="edit(${i})"<i class="fas fa-cog"></i>
-        <a  onClick="supprimer(${i})" <i class="fas fa-ban"></i></a>       
-    </div>`
-        
-    })
-}
-
 
 function onTaskSubmit(){
 
@@ -52,7 +37,15 @@ function supprimer(i){
 }
 
 function edit(i){
+
+    var modifier = document.getElementsByClassName("modifier")[i]
     
+    
+    modifier.innerHTML = modifier.innerHTML + `
+    <form >
+    <input  value="" id="input"  type="text" placeholder="A faire..."><br>
+    <button id="bouton">valider</button>
+    </form>`
     
     tasks[i].value = document.getElementById("inputBis").value
     displayList()
