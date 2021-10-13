75b3c1c8cd1b6de0e0a9885fa4479245fc620468
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-10-13 15:33:02 +0200

    fonction supprimer

--- index.js
+++ index.js
@@ -5,31 +5,38 @@ var todoContainer = document.getElementsByClassName("todoContainer");
 
 var tasks = []
 
-
-function onTaskSubmit(){
-
-    var inputValue = document.getElementById("input").value
-    var todo = { value: inputValue, status: "to do" }
-
-    
-    tasks.push(todo)
-
+function displayList(){
     list.innerHTML = ``
 
-    tasks.forEach(function(todo, index){
+    tasks.forEach(function(todo, i){
         list.innerHTML = list.innerHTML + `
         <div class="toDoList">
         <button class="Check"> check ! </button>
         <h4 class="toDoStatus">${todo.status}</h4>
         <p class="toDoTexte">${todo.value}</p>
-        <i class="fas fa-cog"></i>
-        <a <i onClick="supprimer()" class="fas fa-ban"></i></a>       
+        <a onClick="edit(${i})"<i class="fas fa-cog"></i>
+        <a  onClick="supprimer(${i})" <i class="fas fa-ban"></i></a>       
     </div>`
         
     })
-
-
-
 }
 
+function onTaskSubmit(){
+
+    var inputValue = document.getElementById("input").value
+    var todo = { value: inputValue, status: "to do" }
+    tasks.push(todo)
+    displayList()
+}
+
+function supprimer(i){
+    tasks.splice(i, 1)
+    displayList()
+}
+
+function edit(i){
+
+    tasks[i]
+    displayList()
+}
 
